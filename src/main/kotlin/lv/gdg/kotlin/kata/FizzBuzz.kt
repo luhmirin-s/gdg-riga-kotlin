7c4e3ee33d163b84ed97db8c324bd14b3fdc298f
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-12-17 12:02:16 +0300

    First FizzBuzz commit

---
+++ src/main/kotlin/lv/gdg/kotlin/kata/FizzBuzz.kt
@@ -0,0 +1,8 @@
+package lv.gdg.kotlin.kata
+
+fun fizzBuzz(num: Int): String {
+    return when (num) {
+        else -> num.toString()
+    }
+}
+
---
+++ src/test/kotlin/lv/gdg/kotlin/kata/FizzBuzzKtTest.kt
@@ -0,0 +1,15 @@
+package lv.gdg.kotlin.kata
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FizzBuzzKtTest {
+
+//    assertEquals ("Fizz", fizzBuzz(3))
+//    assertEquals ("Buzz", fizzBuzz(5))
+//    assertEquals ("FizzBuzz", fizzBuzz(5))
+    @Test fun shouldPrintNumber() {
+        assertEquals ("1", fizzBuzz(1))
+    }
+
+}

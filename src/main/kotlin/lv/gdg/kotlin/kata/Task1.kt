22c810f24d5ed63b6a1f6b9132c6081fe89644fb
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-12-17 10:59:19 +0300

    Init.

---
+++ src/main/kotlin/lv/gdg/kotlin/kata/Task1.kt
@@ -0,0 +1,17 @@
+package lv.gdg.kotlin.kata
+
+/**
+ * Write a program to show that the 13th day of the month
+ * falls more often on a Friday than any other day of the
+ * week. The 1st of January 1973 was a Monday.
+ * You should aim at producing the clearest possible
+ * program, not the fastest.
+ *
+ * A leap year is defined as one that is divisible by 4,
+ * but is not otherwise divisible by 100 unless it is
+ * also divisible by 400.
+ *
+ * For example, 2001 is a typical common year and 1996
+ * is a typical leap year, whereas 1900 is an atypical
+ * common year and 2000 is an atypical leap year.
+ */

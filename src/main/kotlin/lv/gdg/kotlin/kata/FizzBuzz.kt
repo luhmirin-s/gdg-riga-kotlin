551c625dcb61e4350d692c5c65c135c6602bf066
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-12-17 12:08:37 +0300

    Fizz Buzz working

--- src/main/kotlin/lv/gdg/kotlin/kata/FizzBuzz.kt
+++ src/main/kotlin/lv/gdg/kotlin/kata/FizzBuzz.kt
@@ -2,7 +2,9 @@ package lv.gdg.kotlin.kata
 
 fun fizzBuzz(num: Int): String {
     return when {
+        num % 15 == 0 -> "FizzBuzz"
         num % 3 == 0 -> "Fizz"
+        num % 5 == 0 -> "Buzz"
         else -> num.toString()
     }
 }
--- src/test/kotlin/lv/gdg/kotlin/kata/FizzBuzzKtTest.kt
+++ src/test/kotlin/lv/gdg/kotlin/kata/FizzBuzzKtTest.kt
@@ -5,13 +5,19 @@ import org.junit.Test
 
 class FizzBuzzKtTest {
 
-//    assertEquals ("Buzz", fizzBuzz(5))
-//    assertEquals ("FizzBuzz", fizzBuzz(5))
     @Test fun shouldPrintNumber() {
         assertEquals ("1", fizzBuzz(1))
     }
 
     @Test fun shouldPrintFizz() {
         assertEquals ("Fizz", fizzBuzz(3))
+    }
+
+    @Test fun shouldPrintBuzz() {
+        assertEquals("Buzz", fizzBuzz(5))
+    }
+
+    @Test fun shouldPrintFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz(30))
     }
 }
